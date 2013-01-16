eaccd56e2475aab7dac34b05aa67cd0f32f7ee1c
derek <derekriendeau@yahoo.ca>
2013-01-16 14:12:00 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PilotfilesApp::Application.config.secret_token = '5c8facff02898d1c3b1875d181435b1a9e7f4c7a36c00498baabf58e6f21ea0dd799c7b12c102ee926ddd98e4e101f418d9d48275a4feb4cb87d8f6d5184e1cb'
